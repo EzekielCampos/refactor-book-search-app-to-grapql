3370233100f7330862143286c62b5ca01aad83bc
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-23 16:23:36 -0700

    completed the login mutation and added a .env file for the secret in the authorization for the token

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,19 +1,50 @@
-const { Query } = require("mongoose");
 const { User } = require("../models");
+
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find();
+      try {
+        return User.find();
+      } catch (error) {
+        throw error;
+      }
     },
 
     me: async (root, args, context) => {
-      if (context.user) {
-        return User.findById(context.user._id);
+      try {
+        if (context.user) {
+          return User.findById(context.user._id);
+        }
+        throw AuthenticationError;
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+
+  Mutation: {
+    login: async (root, { email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw AuthenticationError;
+        }
+
+        const verifyPw = await user.isCorrectPassword(password);
+
+        if (!verifyPw) {
+          throw AuthenticationError;
+        }
+
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        res.status(400).json({ message: error });
       }
     },
   },
 };
 
-
-module.exports = resolvers
+module.exports = resolvers;

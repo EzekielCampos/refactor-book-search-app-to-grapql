3da451be54552615dc16ee4538b02091f688de27
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-23 14:00:08 -0700

    added the query type to get users and mutation to create an account and login

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -21,4 +21,15 @@ type Book {
         user:User
     }
 
+    type Query {
+    users:[User]
+    me:User
+    }
+
+    type Mutation {
+    login(email:String!, password:String!):Auth
+    addUser(username:String! ,email:String!, password:String!):Auth)
+
+    }
+
 `;

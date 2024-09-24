51a6d75a82a068d1be19d1ad7eb86b9e97f17037
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-23 21:55:23 -0700

    added the mutations for addUser and addBook

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -37,13 +37,39 @@ const resolvers = {
         if (!verifyPw) {
           throw AuthenticationError;
         }
-
         const token = signToken(user);
         return { token, user };
       } catch (error) {
         res.status(400).json({ message: error });
       }
     },
+    addUser: async (root, args) => {
+      try {
+        const profile = User.create({ ...args });
+        const token = token.sign(profile);
+        return { token, profile };
+      } catch (error) {
+        throw error;
+      }
+    },
+    saveBook: async (root, args, context)=>{
+      try {
+
+        return User.findByIdAndUpdate(context.user._id,
+          {
+            $addToSet:{savedBooks:{...args}}
+          },
+          {
+            new:true,
+            runValidators:true
+          }
+        )
+        
+      } catch (error) {
+        throw error
+        
+      }
+    }
   },
 };
 

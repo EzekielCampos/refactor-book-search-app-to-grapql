21c31539989c0f99fd4c50802c1161437a893740
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-10-16 21:02:00 -0700

    updated resolvers context to include the res and the add user resolver to have cookie instead of sending token directly

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -11,13 +11,13 @@ const resolvers = {
     // The me query will uses the context parameter to fetch the data
     // of the user that is currently logged from the token
     // This will be used mainly to display there saved books
-    me: async (parent, args, context) => {
+    me: async (parent, args, { req }) => {
       try {
-        console.log("Context", context.user);
+        console.log("Context", req.user);
         // Verifies that context is not null
-        if (context.user) {
+        if (req.user) {
           // If user is logged in send all their data to the front-end
-          return User.findOne({ _id: context.user._id });
+          return User.findOne({ _id: req.user._id });
         }
         // If there no user logged in throw an authentification error
         throw AuthenticationError;
@@ -60,29 +60,36 @@ const resolvers = {
     },
 
     // This mutation is used to create a new user profile
-    addUser: async (parent, args) => {
+    addUser: async (parent, args, { res }) => {
       try {
         // Take the properties inside of args and use it to create User account
         const user = await User.create({ ...args });
         // Once the account is created, use that information to create a token
         const token = signToken(user);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "Strict",
+          maxAge: 3600000,
+        });
+
         // Once the token and user account is created they are signed in
-        return { token, user };
+        return { user };
       } catch (error) {
         throw new Error(error.message);
       }
     },
 
     // This saves a book to the user field of savedBooks
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, args, { req }) => {
       // Verify that a user is signed by checking the context parameter
-      if (!context.user) {
+      if (!req.user) {
         throw new AuthenticationError("You need to be logged in!");
       }
       // From there we look up the account using the id from context
       try {
         return User.findByIdAndUpdate(
-          context.user._id,
+          req.user._id,
           // we add the new saved books by destructing all of the properties from args so
           // that it will create a book to be saved to this field
           { $addToSet: { savedBooks: { ...args } } },
@@ -94,10 +101,10 @@ const resolvers = {
     },
 
     // This mutation removes a book from the user field of savedBooks
-    removeBook: async (parent, args, context) => {
+    removeBook: async (parent, args, { req }) => {
       // First checkts to see if the user is logged in by seeing if
       // context has the user property
-      if (!context.user) {
+      if (!req.user) {
         throw new AuthenticationError("You need to be logged in!");
       }
 
@@ -107,7 +114,7 @@ const resolvers = {
         // to remove the book by finding the bookId that was passed
         // in the arguments
         return User.findByIdAndUpdate(
-          context.user._id,
+          req.user._id,
           { $pull: { savedBooks: { bookId: args.bookId } } },
           { new: true, runValidators: true }
         );
